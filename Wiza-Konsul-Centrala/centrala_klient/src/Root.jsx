7b017c7440bbab38bcec49faea8f20a26131296b
Przemek Szeszko <przemekszeszko1@gmail.com>
2019-12-19 01:28:25 +0100

    404 do rozwiązania

--- Wiza-Konsul-Centrala/centrala_klient/src/Root.jsx
+++ Wiza-Konsul-Centrala/centrala_klient/src/Root.jsx
@@ -4,17 +4,32 @@ import axios from 'axios';
 import SprawyList from './components/SprawyList';
 
 
-class Root extends React.Component {
-    
+class Root extends React.Component {   
+    constructor(props) {
+        super(props);
+        this.state = {lista:axios.get('/sprawy')}
+      }
+      async UNSAFE_componentWillMount()
+      {
+        //this.setState({lista: axios.get('/sprawy')});
+      }
         async componentDidMount() {
             axios.defaults.baseURL = 'http://localhost:5000/api';
+         
+            console.log("tu jestem");
+            
         }
+        
+           
+      
         render() {
-           const list=[{"id":1,"imie":"Jan","nazwisko":"Kowalski"}];
+            
+           console.log(this.state.lista);
+          
             return (
                 <div>
             <Form/>
-            <SprawyList sprawy={list}/>
+            <SprawyList sprawy={this.state.lista}/>
             </div>
             
         )};
